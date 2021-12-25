2ee00f13c5db19c1565675a1e6e444be9f66edeb
linshaoneng <linshaoneng@sina.cn>
2021-12-25 22:48:17 +0800

    增加连续扫描多张

---
+++ readme.md
@@ -0,0 +1,7 @@
+1、根据你当前服务器和python的版本，选择twain插件
+https://www.lfd.uci.edu/~gohlke/pythonlibs/#twainmodule
+例如我当前python3.8
+
+通过pip install twain‑1.0.4‑pp38‑pypy38_pp73‑win_amd64.whl安装
+
+2、常见问题
---
+++ twain_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# 林少能 linshaoneng@sina.cn
+# 基于twain扫描仪程序
+# Scanner program based on Twain
+
+from libs import twain
+
+# 扫描一张
+# Scan one
+try:
+    twain.acquire(r'D:/scan/test.png', ds_name='SPK8021',
+                  dpi=300, pixel_type="color")
+except Exception as e:
+    print(e)
+
+# 连续扫描多张
+# Continuous scanning of multiple sheets
+try:
+    twain.acquires(r'D:/scan/', '.png', ds_name='SPK8021',
+                   dpi=300, pixel_type="color")
+except Exception as e:
+    # ConditionCode = 29 扫描仪没有纸/There is no paper in the scanner
+    # ConditionCode = 24 扫描仪没有合好/扫描仪没有合好
+    print(str(e) == "ConditionCode = 29")
